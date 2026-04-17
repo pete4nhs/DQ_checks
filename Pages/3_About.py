0fd8e8d9c6773dba6313ecb7006bc9ee7ab93aa4
pete4nhs <165719339+pete4nhs@users.noreply.github.com>
2026-04-17 17:39:25 +0100

    Update About page text and contact information

--- Pages/3_About.py
+++ Pages/3_About.py
@@ -1,7 +1,7 @@
-import streamlit as st
+#import streamlit as st
 
 st.title('About')
-st.write('This tool is aimed at supporting trusts to run DQ checks themselves for the following reports:')
+st.write('This tool is designed to support trusts in running their own data quality checks for the following reports:')
 st.write('- Indicative Activity Plan (IAP) reporting')
 st.write('- Local Price reporting')
 st.write('')
@@ -9,7 +9,9 @@ st.write('The reports specification can be found at:')
 st.write('- https://www.england.nhs.uk/publication/iap-reporting-specification-technical-detail-specific-data-requirements/')
 st.write('- https://www.england.nhs.uk/publication/local-prices-reporting-specification-technical-detail-specific-data-requirements/')
 st.write('')
-st.write('For any information on the process you can contact the **NHS England London Spec Comm BI team** at: england.dcmd.bilondon@nhs.net ')
+st.write('For any information on the process you can contact the **NHS England London Spec Comm BI team** at: england.dcmd.bilondon@nhs.net or melanietunmore@nhs.net')
+st.write('To report any issue or provide feedback on the application, please contact peter.saiu@nhs.net')
 st.write('')
 st.warning("**Please note that uploading and processing DQ checks through this tool does not constitute data submission. " \
 "This tool is solely intended to assess the formatting of your file.**")
+
